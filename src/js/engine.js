c7e276917e08152c569a519fbde551e1c4c0fca5
gabriel <dgabrielfof@gmail.com>
2023-11-22 17:22:47 -0300

    adding a bit of responsiveness to play on mobile

--- src/js/engine.js
+++ src/js/engine.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js';
-import { getFirestore, addDoc, collection } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js';
+import { getFirestore, addDoc, collection, getDocs } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js';
 
 const state = {
   view: {
@@ -16,11 +16,15 @@ const state = {
     level: document.querySelector("#level"),
     // Display (Game Over)
     endGameMessage: document.querySelector("#result"),
+    // Display (Ranking)
+    dots: document.querySelector("#dots"),
+    ranking: document.querySelector(".ranking"),
     // Pages
     startPage: document.querySelector(".start-page"),
     gamePage: document.querySelector(".game-page"),
     continuePage: document.querySelector(".continue-page"),
     gameOverPage: document.querySelector(".game-over-page"),
+    waitingPage: document.querySelector(".waiting-page"),
     statsPage: document.querySelector(".stats-page"),
     rulesPage: document.querySelector(".rules-page"),
     // Buttons
@@ -29,7 +33,8 @@ const state = {
     restartButton: document.querySelector(".restart"),
     statsButton: document.querySelector(".stats"),
     rulesButton: document.querySelector(".rules"),
-    closeBurron: document.querySelector(".close")
+    rulesCloseButton: document.querySelector(".rule-close"),
+    statsCloseButton: document.querySelector(".stats-close"),
   },
   values: {
     level: 1,
@@ -43,9 +48,12 @@ const state = {
     enemyVelocity: 1000,
     countDownTimerId: 1000,
     hitPosition: 0,
-    currentTime: 5,
+    currentTime: 15,
     missClicks: 0,
-    matchTime: 5,
+    matchTime: 15,
+    rankingList: [],
+    dots: 0,
+    dotInterval: null,
   }
 }
 const firebaseConfig = {
@@ -60,17 +68,16 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const coll = "SCORES"
 
-async function FBFetchData (setFunction, ...args) {
-  const collectionName = args.join('/')
+async function FBFetchData () {
   try{
-    const collectionRef = collection(db, collectionName);
+    const collectionRef = collection(db, coll);
     const snapshot = await getDocs(collectionRef);
 
     const dataFromFirestore = snapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
-    setFunction(dataFromFirestore);
+    state.values.rankingList = dataFromFirestore;
   } catch (error) {
     console.error('Erro ao buscar dados:', error);
   }
@@ -86,10 +93,7 @@ function countDown() {
     clearInterval(state.values.countDownTimerId)
     clearInterval(state.values.timerId)
     state.values.currentTime = state.values.matchTime;
-    accelerateTheEnemy(clean=false)
-
-    // clearInterval(state.actions.timerId)
-    // clearInterval(state.actions.countDownTimerId);
+    accelerateTheEnemy(false)
   }
 }
 function accelerateTheEnemy(clean=false) {
@@ -175,22 +179,79 @@ function moveEnemy() {
 function startClock() {
   state.values.countDownTimerId = setInterval(countDown, 1000)
 }
+function changePage(previousPage, currentPage) {
+  state.view[previousPage].style.display="none";
+  state.view[currentPage].style.display="flex";
+}
 function playGame(currentPage) {
   changePage(currentPage, "gamePage")
   state.values.playerName = state.view.playerName.value || null;
   moveEnemy();
   startClock();
 }
-function changePage(previousPage, currentPage) {
-  state.view[previousPage].style.display="none";
-  state.view[currentPage].style.display="flex";
-}
+function backToMenu() {
+  state.view.statsPage.style.display="none";
+  state.view.rulesPage.style.display="none";
 
+  state.view.startPage.style.display="flex";
+}
+function ranking() {
+  clearInterval(state.values.dotInterval)
+  changePage("waitingPage", "statsPage");
+  let topTenRanking = state.values.rankingList.sort((a, b) => {
+    if (a.score < b.score) {
+      return 1;
+    }
+    if (a.score > b.score) {
+      return -1;
+    }
+    if (a.score === b.score) {
+      if (a.date > b.date) {
+        return 1;
+      }
+      if (a.date < b.date) {
+        return -1;
+      }
+    }
+    return 0
+  }).slice(0, 10)
+  topTenRanking.forEach((result, index) => {
+    const item = document.createElement("li");
+
+    const positionName = document.createElement("h3");
+    positionName.textContent = `#${index +1} ${result.playerName}:`;
+    item.appendChild(positionName)
+    
+    const score = document.createElement("h3");
+    score.textContent = result.score
+    item.appendChild(score)
+
+    state.view.ranking.appendChild(item);
+  })
+}
+function changeDots() {  
+  const dots = [".", "..", "...", " "];
+  console.log(state.view)
+  state.values.dots++
+  state.view.dots.textContent=dots[state.values.dots+1%4]
+}
+async function waiting() {
+  changePage("startPage", "waitingPage");
+  state.values.dotInterval = setInterval(changeDots, 500);
+  
+  await FBFetchData();
+  
+  ranking()
+}
 function init() {
   state.view.lives.textContent = state.values.lives
   state.view.startButton.addEventListener("click", e => playGame("startPage"))
   state.view.continueButton.addEventListener("click", e => playGame("continuePage"))
   state.view.restartButton.addEventListener("click", e => restart())
+  state.view.statsButton.addEventListener("click", e => waiting())
+  state.view.rulesButton.addEventListener("click", e => changePage("startPage", "rulesPage"))
+  state.view.statsCloseButton.addEventListener("click", e => backToMenu())
+  state.view.rulesCloseButton.addEventListener("click", e => backToMenu())
   addListennerHitBox()
   // Fazer uma página de rank
   // fazer o Ralph desaparacer e aparecer
